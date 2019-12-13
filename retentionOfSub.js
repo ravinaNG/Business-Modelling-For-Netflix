8369e9c72b2294f31d8f2507a13ce3275e85eb2a
ravinaNG <ravina18@navgurukul.org>
2019-12-13 10:06:33 +0530

    Node.js Business modelling Netflix (Team)

---
+++ retentionOfSub.js
@@ -0,0 +1,31 @@
+function retentionListOfSubs(){
+    const salesPerson = 1;
+    const trailPerson = 20;
+    const noShow = 40;
+    const numberOfWorkingDays = 28;
+    const dicOfSubscribedList = {};
+    let month = 1
+
+    for(month; month<=60; month++){
+        sinUp = salesPerson*trailPerson*numberOfWorkingDays;
+        const firstSubs = sinUp-(sinUp*noShow/100)
+        const subscribedList = []
+        const retentionList = [85,75,65,60,57,54,51,49,46,43,41,38,36,34,32,30,28,26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,5,4,4,4,3,3,3,3,3,3,2,2,2,2,2,2,2,2,2,2,2,2,2,2]
+        let retentionM = 0;
+        subscribedList.push(firstSubs);
+
+        for (retentionM; retentionM<retentionList.length; retentionM++){
+            const retentionOfMonth = firstSubs*retentionList[retentionM]/100;
+            const intoInt = Math.floor(retentionOfMonth);
+            subscribedList.push(intoInt);
+        }
+        var str = "month" + month;
+        dicOfSubscribedList[str] = subscribedList;
+    }
+    return dicOfSubscribedList;
+}
+
+const listOfRetentions = retentionListOfSubs();
+// console.log(typeof(listOfRetentions));
+// console.log(JSON.stringify(listOfRetentions));
+module.exports.listOfRetentions = listOfRetentions;
---
+++ revenueOfSalesP.js
@@ -0,0 +1,28 @@
+let retentions = require('./retentionOfSub')
+
+console.log(retentions);
+// const amtPerSubscription = 650;
+// const revenue = 40;
+// const year = 5;
+// const inflationRate = 6;
+
+// let rowM = 0;
+// let columnM = 0;
+// let dicOfRevenue = {};
+
+// for(columnM; columnM<60; columnM++){
+//     let numberOfSubscriber = 0;
+//     let revenueOfMoth = 0;
+//     let month = 'month';
+//     rowM = columnM;
+//     let count = 1;
+//     for(rowM; rowM >= 0; rowM --){
+//         month = "month" + count;
+//         numberOfSubscriber = numberOfSubscriber + retentions['listOfRetentions'][month][rowM];
+//         count = count + 1;
+//     }
+//     revenueOfMoth = numberOfSubscriber*amtPerSubscription*revenue/100;
+//     dicOfRevenue[month] = revenueOfMoth;
+// }
+
+// // console.log(dicOfRevenue);
