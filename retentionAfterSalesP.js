1a4318a68ce5ea48e33371727ce424a90d1b8526
ravinaNG <ravina18@navgurukul.org>
2020-01-12 12:16:14 +0530

    made one more file of retentions of after sales persion

---
+++ retentionAfterSalesP.js
@@ -0,0 +1,32 @@
+const detailsOfSalesP = require('./retentionOfSub').detailsOfSalesP;
+
+const retentionList = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 4, 4, 4, 5, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 28, 30, 32, 34, 36, 38, 40, 43, 46, 49, 51, 54, 57, 60, 65, 75, 85, 100]
+
+function retentionAfterSalesP(details){
+    const salesMan = details['salesMan'];
+    const trailPersons = details['trailPersons'];
+    const numOfWorkingDays = details['numOfWorkingDays'];
+    const noShowPerMonth = details['noShowPerMonth'];
+
+    sinUp = salesMan*trailPersons*numOfWorkingDays;
+    const firstSubs = sinUp-(sinUp*noShowPerMonth/100);
+
+    const allMonths = {};
+    let startingMonth = 0;
+    let numOfMonth = 61;
+    for(startingMonth; startingMonth<retentionList.length; startingMonth++){
+        const retentionsOfMonth = [];
+        let retention = startingMonth;
+        for(retention; retention>0; retention--){
+            subscribers = parseInt(firstSubs*retentionList[retention]/100);
+            retentionsOfMonth.push(subscribers);
+        }
+        var month = "month" + numOfMonth;
+        allMonths[month] = retentionsOfMonth;
+        numOfMonth++;
+    }
+    return allMonths;
+}
+
+const listOfRetentions = retentionAfterSalesP(detailsOfSalesP);
+console.log(listOfRetentions);
--- retentionOfSub.js
+++ retentionOfSub.js
@@ -1,23 +1,39 @@
 const readline = require('readline-sync');
 
-function retentionListOfSubs(){
+function details(){
+    const details = {};
     const salesMan = parseInt(readline.question("How many sales persons are working? :- "));
     const trailPersons = parseInt(readline.question('How many sign ups are there in a day? :- '));
     const noShowPerMonth = parseInt(readline.question('what is the percentage of no show? :- '));
     const numOfWorkingDays = parseInt(readline.question('Working Days? :- '));
-    
+
+    details['salesMan'] = salesMan;
+    details['trailPersons'] = trailPersons;
+    details['noShowPerMonth'] = noShowPerMonth;
+    details['numOfWorkingDays'] = numOfWorkingDays;
+
+    return details;
+}
+
+function retentionListOfSubs(detailsOfSalesP){
+    const retentionList = [100, 85,75,65,60,57,54,51,49,46,43,40,38,36,34,32,30,28,26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,5,4,4,4,3,3,3,3,3,3,2,2,2,2,2,2,2,2,2,2]
+
     const dicOfSubscribedList = {};
     let month = 1
+    const salesMan = detailsOfSalesP['salesMan'];
+    const trailPersons = detailsOfSalesP['trailPersons'];
+    const numOfWorkingDays = detailsOfSalesP['numOfWorkingDays'];
+    const noShowPerMonth = detailsOfSalesP['noShowPerMonth'];
+
+    sinUp = salesMan*trailPersons*numOfWorkingDays;
+    const firstSubs = sinUp-(sinUp*noShowPerMonth/100)
 
     for(month; month<=60; month++){
-        sinUp = salesMan*trailPersons*numOfWorkingDays;
-        const firstSubs = sinUp-(sinUp*noShowPerMonth/100)
         const subscribedList = []
-        const retentionList = [85,75,65,60,57,54,51,49,46,43,40,38,36,34,32,30,28,26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,5,4,4,4,3,3,3,3,3,3,2,2,2,2,2,2,2,2,2,2]
         let retentionM = 0;
-        subscribedList.push(firstSubs);
-
-        for (retentionM; retentionM<retentionList.length; retentionM++){
+        const lengthOfRetentionList = retentionList.length - (month-1)
+        
+        for (retentionM; retentionM<lengthOfRetentionList; retentionM++){
             const retentionOfMonth = firstSubs*retentionList[retentionM]/100;
             const intoInt = Math.floor(retentionOfMonth);
             subscribedList.push(intoInt);
@@ -28,9 +44,11 @@ function retentionListOfSubs(){
     return dicOfSubscribedList;
 }
 
-const listOfRetentions = retentionListOfSubs();
+const detailsOfSalesP = details();                                      
+
+const listOfRetentions = retentionListOfSubs(detailsOfSalesP);
 // console.log(listOfRetentions);
 // console.log(typeof(listOfRetentions));
 // console.log(JSON.stringify(listOfRetentions));
-module.exports.listOfRetentions = listOfRetentions;
+module.exports = {detailsOfSalesP, listOfRetentions}
 
